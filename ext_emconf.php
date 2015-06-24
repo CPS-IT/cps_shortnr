7483da5c373c272ebb9292a4a5822c68c135265e
Nicole Cordes <cordes@cps-it.de>
2015-06-24 17:22:56 +0200

    [RELEASE] Release of cps_shortnr 0.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "cps_shortnr".
  *
- * Auto generated 24-06-2015 15:49
+ * Auto generated 24-06-2015 17:22
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
 	'author_company' => '',
-	'version' => '0.1.0',
+	'version' => '0.2.0',
 	'constraints' => 
 	array (
 		'depends' => 
@@ -34,6 +34,6 @@ $EM_CONF[$_EXTKEY] = array (
 		array (
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"41d9";s:21:"ext_conf_template.txt";s:4:"a48e";s:12:"ext_icon.gif";s:4:"5349";s:17:"ext_localconf.php";s:4:"b582";s:45:"Classes/Controller/PageNotFoundController.php";s:4:"8ed4";}',
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"5f72";s:21:"ext_conf_template.txt";s:4:"9d51";s:12:"ext_icon.gif";s:4:"5349";s:17:"ext_localconf.php";s:4:"b582";s:45:"Classes/Controller/PageNotFoundController.php";s:4:"6a49";s:25:"Resources/cps_shortnr.txt";s:4:"91e3";}',
 );
 
