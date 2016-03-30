701db9a96ef15192c362efe6974221273815ce0c
Nicole Cordes <cordes@cps-it.de>
2016-03-30 22:36:58 +0200

    [RELEASE] Release of cps_shortnr 0.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "cps_shortnr".
  *
- * Auto generated 12-08-2015 15:14
+ * Auto generated 30-03-2016 22:36
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -21,20 +21,20 @@ $EM_CONF[$_EXTKEY] = array (
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
 	'author_company' => '',
-	'version' => '0.4.0-dev',
-	'constraints' =>
+	'version' => '0.4.0',
+	'constraints' => 
 	array (
-		'depends' =>
+		'depends' => 
 		array (
 			'typo3' => '4.5.0-6.2.99',
 		),
-		'conflicts' =>
+		'conflicts' => 
 		array (
 		),
-		'suggests' =>
+		'suggests' => 
 		array (
 		),
 	),
-	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"776c";s:21:"ext_conf_template.txt";s:4:"9d51";s:12:"ext_icon.gif";s:4:"5349";s:17:"ext_localconf.php";s:4:"b582";s:45:"Classes/Controller/PageNotFoundController.php";s:4:"0127";s:25:"Resources/cps_shortnr.txt";s:4:"e87d";}',
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"091d";s:21:"ext_conf_template.txt";s:4:"9d51";s:12:"ext_icon.gif";s:4:"5349";s:17:"ext_localconf.php";s:4:"b582";s:45:"Classes/Controller/PageNotFoundController.php";s:4:"8ecf";s:25:"Resources/cps_shortnr.txt";s:4:"e87d";}',
 );
 
