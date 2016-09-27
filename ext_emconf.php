1b9a45909408cd7362f73a46c7190a3ff96f07a8
Nicole Cordes <cordes@cps-it.de>
2016-09-27 12:05:24 +0200

    [RELEASE] Release of cps_shortnr 0.4.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "cps_shortnr".
  *
- * Auto generated 30-03-2016 22:36
+ * Auto generated 27-09-2016 12:04
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -11,31 +11,31 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array (
-	'title' => 'Url Shortener',
-	'description' => 'Builds links to pages and extension records with a tiny url',
-	'category' => 'fe',
-	'author' => 'Nicole Cordes',
-	'author_email' => 'cordes@cps-it.de',
-	'state' => 'beta',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'author_company' => '',
-	'version' => '0.4.0',
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'typo3' => '4.5.0-6.2.99',
-		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		    'pagenotfoundhandling' => '',
-		),
-	),
-	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"091d";s:21:"ext_conf_template.txt";s:4:"9d51";s:12:"ext_icon.gif";s:4:"5349";s:17:"ext_localconf.php";s:4:"b582";s:45:"Classes/Controller/PageNotFoundController.php";s:4:"8ecf";s:25:"Resources/cps_shortnr.txt";s:4:"e87d";}',
+  'title' => 'Url Shortener',
+  'description' => 'Builds links to pages and extension records with a tiny url',
+  'category' => 'fe',
+  'author' => 'Nicole Cordes',
+  'author_email' => 'cordes@cps-it.de',
+  'state' => 'beta',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'author_company' => '',
+  'version' => '0.4.1',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '4.5.0-6.2.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+      'pagenotfoundhandling' => '',
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"0afb";s:21:"ext_conf_template.txt";s:4:"9d51";s:12:"ext_icon.gif";s:4:"5349";s:17:"ext_localconf.php";s:4:"b582";s:45:"Classes/Controller/PageNotFoundController.php";s:4:"8ecf";s:25:"Resources/cps_shortnr.txt";s:4:"e87d";}',
 );
 
